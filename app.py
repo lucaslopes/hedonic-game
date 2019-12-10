dfc6ccb793ff494115c2df6b093b93200acb2b58
Lucas Lopes <lucaslopesf2@gmail.com>
2019-12-10 01:24:01 -0300

    Deploy to Heroku

--- app.py
+++ app.py
@@ -330,6 +330,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 app.layout = html.Div(children=[
         Header,RunExperiments,
         TableResults,InstantGain,AccumulatedGain,PotentialProportion,VerticesProportion,EdgesProportion])
