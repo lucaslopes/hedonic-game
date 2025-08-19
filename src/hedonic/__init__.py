d60a8593c6fc38e48b5772d6115ee48266063613
Lucas Lopes Felipe <lucaslopesf2@gmail.com>
2025-08-18 22:47:08 -0300

    Integrate igraph library into Game class, enhancing its functionality to represent hedonic games as graphs. Update dependencies in pyproject.toml to include igraph.

--- src/hedonic/__init__.py
+++ src/hedonic/__init__.py
@@ -1,20 +1,33 @@
-class Game:
-    """A class representing a hedonic game."""
-    
-    def __init__(self, name: str = "Default Game"):
-        self.name = name
-        self.players = []
-    
-    def add_player(self, player: str) -> None:
-        """Add a player to the game."""
-        self.players.append(player)
-    
-    def get_players(self) -> list[str]:
-        """Get all players in the game."""
-        return self.players.copy()
-    
-    def __str__(self) -> str:
-        return f"Game: {self.name} with {len(self.players)} players"
+from igraph import Graph  # type: ignore
 
-# Make Game the main export
+
+class Game(Graph):
+    """A hedonic game represented as an `igraph.Graph`."""
+
+    def __init__(self, graph: Graph | None = None, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        if isinstance(graph, Graph):
+            self.add_vertices(graph.vcount())
+            self.add_edges(graph.get_edgelist())
+            for attr in graph.vertex_attributes():
+                self.vs[attr] = graph.vs[attr]
+            for attr in graph.edge_attributes():
+                self.es[attr] = graph.es[attr]
+            for attr in graph.attributes():
+                self[attr] = graph[attr]
+
+    def to_igraph(self) -> Graph:
+        """Convert the `Game` object to a standalone `igraph.Graph` instance."""
+        g = Graph()
+        g.add_vertices(self.vcount())
+        g.add_edges(self.get_edgelist())
+        for attr in self.vertex_attributes():
+            g.vs[attr] = self.vs[attr]
+        for attr in self.edge_attributes():
+            g.es[attr] = self.es[attr]
+        for attr in self.attributes():
+            g[attr] = self[attr]
+        return g
+
+
 __all__ = ["Game"]
